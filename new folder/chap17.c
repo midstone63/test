71cfd49b859de7508fa3c81e931ce2338ea01a59
sjkim <sjkim@srs.ai>
2021-02-01 11:44:18 +0900

    Test
    test.

---
+++ new folder/chap17.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void DoublePointerSwap(int** dp1, int** dp2);
+int main_17(void)
+{
+	// double pointer 
+	/*int num1 = 10, num2 = 20;
+	int* ptr1 = &num1; int* ptr2 = &num2;
+
+	printf("%d, %d\n", *ptr1, *ptr2);
+	DoublePointerSwap(&ptr1, &ptr2);
+	printf("%d, %d\n", *ptr1, *ptr2);*/
+
+	int num1 = 10, num2 = 20, num3 = 30;
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* ptr3 = &num3;
+	int* ptrArr[] = { ptr1, ptr2, ptr3 }; //포인터 배열. 더블 포인터. ptrArr은 int형 더블 포인터가 된다. 
+	int** dptr = ptrArr;
+
+	printf("%d, %d, %d\n", *ptrArr[0], *ptrArr[1], *ptrArr[2]);
+	printf("%d, %d, %d\n", *dptr[0], *dptr[1], *dptr[2]);
+
+
+	return 0;
+}
+
+void DoublePointerSwap(int** dp1, int** dp2) // 이전 예제는 변수값을 바꾸는 것, 이번 예제는 포인터값을 바꾸는 것. 
+{
+	int* tmp = *dp1;
+	*dp1 = *dp2;
+	*dp2 = tmp;
+}
